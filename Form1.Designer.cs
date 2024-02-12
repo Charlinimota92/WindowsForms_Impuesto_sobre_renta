bb71bbbeab635a00023c7526367fca664595cd34
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 19:55:09 -1200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsForms_Impuesto_sobre_renta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_sueldo = new System.Windows.Forms.TextBox();
+            this.txt_AFP = new System.Windows.Forms.TextBox();
+            this.txt_SFS = new System.Windows.Forms.TextBox();
+            this.txt_ISR = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_status = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_Nombre_Empleado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Impuesto sobre la renta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(136, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sueldo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(137, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "AFP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(136, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "SFS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(156, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ISR";
+            // 
+            // txt_sueldo
+            // 
+            this.txt_sueldo.Location = new System.Drawing.Point(233, 136);
+            this.txt_sueldo.Name = "txt_sueldo";
+            this.txt_sueldo.Size = new System.Drawing.Size(100, 20);
+            this.txt_sueldo.TabIndex = 5;
+            // 
+            // txt_AFP
+            // 
+            this.txt_AFP.Location = new System.Drawing.Point(233, 173);
+            this.txt_AFP.Name = "txt_AFP";
+            this.txt_AFP.Size = new System.Drawing.Size(100, 20);
+            this.txt_AFP.TabIndex = 6;
+            // 
+            // txt_SFS
+            // 
+            this.txt_SFS.Location = new System.Drawing.Point(233, 219);
+            this.txt_SFS.Name = "txt_SFS";
+            this.txt_SFS.Size = new System.Drawing.Size(100, 20);
+            this.txt_SFS.TabIndex = 7;
+            // 
+            // txt_ISR
+            // 
+            this.txt_ISR.Location = new System.Drawing.Point(257, 283);
+            this.txt_ISR.Name = "txt_ISR";
+            this.txt_ISR.Size = new System.Drawing.Size(100, 20);
+            this.txt_ISR.TabIndex = 8;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_calcular.Location = new System.Drawing.Point(96, 382);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(85, 52);
+            this.btn_calcular.TabIndex = 9;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_limpiar.Location = new System.Drawing.Point(196, 382);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(85, 52);
+            this.btn_limpiar.TabIndex = 10;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_salir.Location = new System.Drawing.Point(301, 382);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(85, 52);
+            this.btn_salir.TabIndex = 11;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(142, 326);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 26);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Status";
+            // 
+            // txt_status
+            // 
+            this.txt_status.Location = new System.Drawing.Point(257, 332);
+            this.txt_status.Name = "txt_status";
+            this.txt_status.Size = new System.Drawing.Size(100, 20);
+            this.txt_status.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(137, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(196, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Nombre_Empleado";
+            // 
+            // txt_Nombre_Empleado
+            // 
+            this.txt_Nombre_Empleado.Location = new System.Drawing.Point(339, 100);
+            this.txt_Nombre_Empleado.Name = "txt_Nombre_Empleado";
+            this.txt_Nombre_Empleado.Size = new System.Drawing.Size(126, 20);
+            this.txt_Nombre_Empleado.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(517, 469);
+            this.Controls.Add(this.txt_Nombre_Empleado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_status);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_ISR);
+            this.Controls.Add(this.txt_SFS);
+            this.Controls.Add(this.txt_AFP);
+            this.Controls.Add(this.txt_sueldo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_sueldo;
+        private System.Windows.Forms.TextBox txt_AFP;
+        private System.Windows.Forms.TextBox txt_SFS;
+        private System.Windows.Forms.TextBox txt_ISR;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_status;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_Nombre_Empleado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Impuesto_sobre_renta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+           
+            txt_Nombre_Empleado.Clear();
+            txt_sueldo.Clear();
+            txt_AFP.Clear();
+            txt_SFS.Clear();
+            txt_ISR.Clear();
+            txt_status.Clear();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+
+           
+            string nombre = txt_Nombre_Empleado.Text;
+            double sueldo = Convert.ToDouble(txt_sueldo.Text);
+
+            // Calcular el impuesto sobre la renta (ISR) según la tabla de impuestos actual en tu país
+            double isr = CalcularISR(sueldo);
+
+
+            double afp = sueldo * 0.0287; // 2.87%
+            double sfs = sueldo * 0.0304; // 3.04%
+
+           
+            txt_AFP.Text = afp.ToString("0.00");
+            txt_SFS.Text = sfs.ToString("0.00");
+            txt_ISR.Text = isr.ToString("0.00");
+
+            // Determinar el estatus del ISR y mostrarlo en el cuadro de texto correspondiente
+            if (isr == 0)
+            {
+                txt_status.Text = "n/a";
+            }
+            else
+            {
+                txt_status.Text = "apli";
+            }
+        }
+
+        private double CalcularISR(double sueldo)
+        {
+            double isr = 0;
+           
+            isr = sueldo * 0.20;
+
+            return isr;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
